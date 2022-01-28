c1348b781383fd9d9ac7ea0876cd6bdc266964d9
Navod Dilshan <navoddilshan9@gmail.com>
2022-01-28 18:28:20 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Dashboard from './Pages/Home/dashboard'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-// import PropagateLoader from 'react-spinners/PropagateLoader'
+import PropagateLoader from 'react-spinners/PropagateLoader'
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-import React, { Component } from 'react'
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className='App'>
-          <ul>
-            <Link to='/'>Home</Link>
-          </ul>
-          <Routes>
-            <Route exact path='/' element={<Dashboard />}></Route>
-          </Routes>
+function App() {
+  const [loading, setLoading] = useState(false)
+  let [color, setColor] = useState('#ffffff')
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  }, [])
+  return (
+    <div className='App'>
+      {loading ? (
+        <div className='Loader'>
+          <PropagateLoader
+            loading={loading}
+            size={25}
+            margin={2}
+            color='#00AD5F'
+          />
         </div>
-      </Router>
-    )
-  }
+      ) : (
+        <Dashboard />
+      )}
+    </div>
+  )
 }
 
 export default App
