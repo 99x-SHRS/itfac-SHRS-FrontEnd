6fa06b428b5c1954c7e1cd58340e4a375db654e0
Navod Dilshan <navoddilshan9@gmail.com>
2022-01-28 13:52:36 +0530

    reomve loader

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import Dashboard from './Pages/Home/dashboard'
 import React, { useState, useEffect } from 'react'
 import PropagateLoader from 'react-spinners/PropagateLoader'
-import { css } from '@emotion/react'
-import './App.css'
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  let [color, setColor] = useState('#ffffff')
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 4000)
-  }, [])
   return (
     <div className='App'>
-      {loading ? (
-        <div className='Loader'>
-          <PropagateLoader
-            loading={loading}
-            size={25}
-            margin={2}
-            color='#00AD5F'
-          />
-        </div>
-      ) : (
-        <Dashboard />
-      )}
+      <Dashboard />
     </div>
   )
 }
