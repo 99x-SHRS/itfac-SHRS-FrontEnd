62e37f1f63e348dea732892396b6199061e9586e
Navod Dilshan <navoddilshan9@gmail.com>
2022-05-02 23:32:55 +0530

    seller booking age commit

---
+++ src/Pages/Seller/Bookings/Bookings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../../Components/Navbar/navbar.js'
+const Bookings = () => {
+  return (
+    <div>
+      <Navbar />
+      <h4>asd</h4>
+    </div>
+  )
+}
+
+export default Bookings
