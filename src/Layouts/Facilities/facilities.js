5ef89d90f1d4c8393c056c94a75deda1474d8e09
Navod Dilshan <navoddilshan9@gmail.com>
2022-06-02 02:16:17 +0530

    facilites services added

--- src/Layouts/Facilities/facilities.js
+++ src/Layouts/Facilities/facilities.js
@@ -22,8 +22,9 @@ const Facilities = () => {
   const [facilityTypes, setFacilityTypes] = useState(null)
 
   useEffect(() => {
-    setHotel(searchedParams.get('hotel') || '')
-    if (hotel != null) {
+    let hotelId = searchedParams.get('hotel') || ''
+    setHotel(hotelId)
+    if (hotelId != null) {
       getFacilites()
       getFacilityTypes()
     }
@@ -35,8 +36,8 @@ const Facilities = () => {
     }
     await getFacilityTypesByHotelId(dataModel)
       .then((res) => {
-        // console.log(res.data)
-        setFacilities(res.data)
+        console.log(res.data)
+        setFacilityTypes(res.data)
       })
       .catch((err) => {
         console.log(err)
@@ -49,8 +50,9 @@ const Facilities = () => {
     }
     await getAllFacilitiesByHotelId(dataModel)
       .then((res) => {
-        // console.log(res.data)
-        setFacilityTypes(res.data)
+        // console.log('asd')
+        console.log(res.data)
+        setFacilities(res.data)
       })
       .catch((err) => {
         console.log(err)
@@ -58,74 +60,47 @@ const Facilities = () => {
   }
   return (
     <div className='mt-4 pt-4' id='hotel-facilities'>
-      <h3>Facilities of Furnished apartment at Nawala</h3>
-      <div className='container mt-3'>
-        <div className='row mb-3 '>
-          <div className='col-md-4  '>
-            <div className='facility-header'>
-              <i class='fas fa-parking'></i>
-              <p>Parking</p>
-            </div>
-            <div className='mt-2 '>
-              <p>
-                Free private parking is available on site (reservation is not
-                needed).
-              </p>
-              <div className='mt-2'>
-                {facilitiess.map((facility, index) => {
-                  return (
-                    <div className='m-1'>
-                      <i class='fa-solid fa-check'></i>
-                      <span className='ml-2'>{facility}</span>
+      {facilities != null && facilityTypes != null ? (
+        <>
+          <h3>Facilities of Furnished apartment at Nawala</h3>
+          <div className='container mt-3'>
+            <div className='row mb-3 '>
+              {facilityTypes.map((facilityType, index) => {
+                return (
+                  <>
+                    <div className='col-md-4  '>
+                      <div className='facility-header'>
+                        <i class='fas fa-parking'></i>
+                        <p>{facilityType.name}</p>
+                      </div>
+                      <div className='mt-2 '>
+                        <p>{facilityType.description}</p>
+                        <div className='mt-2'>
+                          {facilities.map((facility, index) => {
+                            if (
+                              facility.facilitytypeFacilityTypeId ==
+                              facilityType.facilityTypeId
+                            ) {
+                              return (
+                                <div className='m-1'>
+                                  <i class='fa-solid fa-check'></i>
+                                  <span className='ml-2'>{facility.name}</span>
+                                </div>
+                              )
+                            }
+                          })}
+                        </div>
+                      </div>
                     </div>
-                  )
-                })}
-              </div>
+                  </>
+                )
+              })}
             </div>
           </div>
-          <div className='col-md-4  '>
-            <div className='facility-header'>
-              <div className='facility-header'>
-                <i class='fa-solid fa-wifi'></i>
-                <p>WIFI</p>
-              </div>
-            </div>
-
-            <div>
-              <p>WiFi is available in all areas and is free of charge.</p>
-              <div className='mt-2'>
-                {facilitiess.map((facility, index) => {
-                  return (
-                    <div className='m-1'>
-                      <i class='fa-solid fa-check'></i>
-                      <span className='ml-2'>{facility}</span>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          </div>
-          <div className='col-md-4  '>
-            <div className='facility-header'>
-              <i class='fa-solid fa-utensils'></i>
-              <p>Food & Drink</p>
-            </div>
-            <div>
-              <p>WiFi is available in all areas and is free of charge.</p>
-              <div className='mt-2'>
-                {facilitiess.map((facility, index) => {
-                  return (
-                    <div className='m-1'>
-                      <i class='fa-solid fa-check'></i>
-                      <span className='ml-2'>{facility}</span>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>asd</>
+      )}
     </div>
   )
 }
