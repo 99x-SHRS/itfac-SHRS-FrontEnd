cad9650d9b1ba44e9cfd8210986d83b37c9bd439
Navod Dilshan <navoddilshan9@gmail.com>
2022-06-08 23:50:00 +0530

    file rearranged

--- src/Components/HorizontalLine/HorizontalLine.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import '../../Assets/styles/css/Components/horizontalLine.css'
-class HorizontalLine extends Component {
-  render() {
-    return <div class='fade_rule'></div>
-  }
-}
-
-export default HorizontalLine
--- src/Routes/ExceptionsRouters.js
+++
@@ -1,7 +0,0 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Exception404 from '../Pages/Exception/PageNotFound404'
-import React from 'react'
-
-const exceptionsRouters = <Route path='*'></Route>
-
-export default exceptionsRouters
