330f3d5ac7527293eecf403d52dddfe88d6071fb
Madhawa Munasinghe <madhawaprageeth86@gmail.com>
2022-06-14 12:28:03 +0530

    review Done

--- src/Components/ReviewModel/ReviewModel.js
+++ src/Components/ReviewModel/ReviewModel.js
@@ -24,12 +24,12 @@ const ReviewModel = ({ id, description, image, rating, hotelId, onFresh }) => {
   };
 
   const handleRatingChange = (event) => {
-   setRatings(event.target.value);
+    setRatings(event.target.value);
   };
 
   const handleReviewChange = (event) => {
     setReview(event.target.value);
-   };
+  };
 
   const handleClose = () => {
     setShow(false);
@@ -67,24 +67,24 @@ const ReviewModel = ({ id, description, image, rating, hotelId, onFresh }) => {
   };
 
   const updateReview = async () => {
-    const data ={
-      review: review
-    }
-    if (review != '') {
-      await updateReviewById(updateReviewId,data)
-    .then((response) =>{
-      if (response.status == 200) {
-        notifySuccess("Sucessfully Updated..");
-        handleClose();
-        onFresh();
-      } else {
-        notifyError("An Error Occoured..");
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-    })
-    }else {
+    const data = {
+      review: review,
+    };
+    if (review != "") {
+      await updateReviewById(updateReviewId, data)
+        .then((response) => {
+          if (response.status == 200) {
+            notifySuccess("Sucessfully Updated..");
+            handleClose();
+            onFresh();
+          } else {
+            notifyError("An Error Occoured..");
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
       notifyError("Please enter a review...");
     }
   };
@@ -95,54 +95,57 @@ const ReviewModel = ({ id, description, image, rating, hotelId, onFresh }) => {
   }, []);
 
   return (
-    <div className="rounded shadow border border-primary row g-1 align-middle mb-3 mt-3 p-1">
-      <div className="col-md-3 d-flex justify-content-center my-auto">
-        <img
-          src={hotelData.image}
-          width="170px"
-          height="150px"
-          className="rounded"
-        />
-      </div>
-      <div className="col-md-7 d-flex justify-content-left my-auto">
-        <div className="row">
-          <div className="mt-1 d-inline-flex">
-            <h5 className="">Rating : {rating} </h5>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-star"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-            </svg>
+    <div className="rounded shadow border border-primary row g-1 align-middle mb-3 mt-3 p-1 ">
+      <div className="row">
+        <div className="col-lg-3 d-flex justify-content-center my-auto">
+          <img
+            src={hotelData.image}
+            width="170px"
+            height="130px"
+            className="rounded"
+          />
+        </div>
+        <div className="col-lg-7 d-flex justify-content-left my-auto">
+          <div className="row">
+            <div className="mt-1 d-inline-flex">
+              <h5 className="">Rating : {rating} </h5>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-star"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+              </svg>
+            </div>
+            <p className="mb-1 mt-1">Hotel Name: {hotelData.name}</p>
+            <p className="mb-1">{description}</p>
+            <p className="mb-1">Location : {hotelData.town} </p>
           </div>
-          <p className="mb-1 mt-1">Hotel Name: {hotelData.name}</p>
-          <p className="mb-1">{description}</p>
-          <p className="mb-1">Location : {hotelData.town} </p>
+        </div>
+        <div className="col-lg-2 center-block my-auto mx-auto">
+          <button
+            type="button"
+            className="btn btn-outline-info rounded-pill btn-block mt-1 mb-3"
+            onClick={() => {
+              handleShow();
+              setupdateReviewId(id);
+            }}
+          >
+            Update
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-danger rounded-pill btn-block mb-2"
+            onClick={deleteReview}
+          >
+            Remove
+          </button>
         </div>
       </div>
-      <div className="col-md-2 justify-content-center my-auto mx-auto">
-        <button
-          type="button"
-          className="btn btn-outline-info rounded-pill"
-          onClick={() => {
-            handleShow();
-            setupdateReviewId(id);
-          }}
-        >
-          Update
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-danger rounded-pill"
-          onClick={deleteReview}
-        >
-          Remove
-        </button>
-      </div>
+
       <Modal
         show={show}
         onHide={() => {
@@ -163,7 +166,9 @@ const ReviewModel = ({ id, description, image, rating, hotelId, onFresh }) => {
                 placeholder="ratings.."
                 onChange={handleRatingChange}
               />
-              <label for="floatingInput">How many ratings do you give out of 10</label>
+              <label for="floatingInput">
+                How many ratings do you give out of 10
+              </label>
             </div>
           </div>
           <div>
--- src/Pages/Account/Review.js
+++ src/Pages/Account/Review.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ReactPaginate from 'react-paginate';
 
 import ReviewModel from "../../Components/ReviewModel/ReviewModel.js";
 
@@ -7,68 +8,42 @@ import "../../Assets/styles/css/Pages/reviewPage.css";
 import { getReviewByCustomerId } from "../../Services/Api/Utilities/index.js";
 
 const Review = () => {
+  let limit = 3;
   const [items, setItems] = useState([]);
-  const [itemOffset, setItemOffset] = useState(0);
-  const [showBack, setShowBack] = useState(true);
+  const [pageCount, setpageCount] = useState(0);
 
-  const viewBack = (itemOffset) => {
-    if (itemOffset === 0) {
-      setShowBack(false);
-    } else {
-      setShowBack(true);
-    }
-  };
-  const getReviewDetails = async (page) => {
-    var pages;
-    if (page == null) {
-      pages = itemOffset;
-    } else {
-      pages = page;
-    }
+  useEffect(() => {
+    getReviewDetails(0);
+  }, []);
+
+  const handlePageClick = async (data) => {
+    let currentPage = data.selected
+    getReviewDetails(currentPage)
+  }
+
+  const getReviewDetails = async (currentPage) => {
     const data = {
       id: localStorage.getItem("user"),
-      page: pages,
+      page: currentPage,
     };
     await getReviewByCustomerId(data)
       .then((response) => {
+        console.log(response);
         const data = response.data;
-        setItems(data);
+        setItems(data.rows);
+        setpageCount(Math.ceil(response.data.count / limit));
       })
       .catch((e) => {
         console.log(e);
       });
   };
 
-  useEffect(() => {
-    getReviewDetails(itemOffset);
-    //viewBack(itemOffset);
-  }, []);
 
-  const handlependingNextPageClick = async (event) => {
-    const newOffset = itemOffset + 1;
-    // console.log(
-    //   `User requested page number ${event.selected}, which is offset ${newOffset}`
-    // );
-    setItemOffset(newOffset);
-    getReviewDetails(newOffset);
-  };
-
-  const handlependingBackPageClick = async (event) => {
-    const newOffset = itemOffset - 1;
-    // console.log(
-    //   `User requested page number ${event.selected}, which is offset ${newOffset}`
-    // );
-    setItemOffset(newOffset);
-    getReviewDetails(newOffset);
-  };
 
   return (
     <div className="container">
       <div className="row g-3">
         <div className="col-12 shadow-lg rounded-3 border border-secondary ml-2">
-          <div className="mt-3 ml-2">
-            <h3>Your Reviews.....</h3>
-          </div>
           <div className="pl-5 pr-5 pb-2">
             {items.map((item) => {
               return (
@@ -82,26 +57,25 @@ const Review = () => {
               );
             })}
           </div>
-          <div className="d-flex justify-content-center mb-2">
-            {showBack ? (
-              <button
-                className="pending-req-button mx-5"
-                onClick={() => {handlependingBackPageClick();
-                  //viewBack(itemOffset);
-                }}
-              >
-                Back
-              </button>
-            ) : (<></>)}
-            <button
-              className="pending-req-button mx-5"
-              onClick={() => {handlependingNextPageClick();
-                //viewBack(itemOffset);
-              }}
-            >
-              Next
-            </button>
-          </div>
+          <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            pageCount={pageCount}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={2}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination justify-content-center"}
+            pageClassName={"page-item"}
+            pageLinkClassName={"page-link"}
+            previousClassName={"page-item"}
+            previousLinkClassName={"page-link"}
+            nextClassName={"page-item"}
+            nextLinkClassName={"page-link"}
+            breakClassName={"page-item"}
+            breakLinkClassName={"page-link"}
+            activeClassName={"active"}
+          />
         </div>
       </div>
     </div>
